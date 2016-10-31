ace2e65268b740377d8e66ec2a75f5138c4d498c
Julio Gonzalez <julio.gteran@gmail.com>
2016-10-31 12:43:18 +0000

    first user story

--- app/app.rb
+++ app/app.rb
@@ -2,8 +2,12 @@ require 'sinatra/base'
 
 class MakersBnb < Sinatra::Base
   get '/' do
-    'Hello MakersBnb!'
+    erb :index
   end
+
+  # get '/spaces' do
+  #   erb :spaces
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0

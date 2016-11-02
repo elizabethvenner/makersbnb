4cfa074cc9a537bc9d5d0ff9de3240e977ab1035
Julio Gonzalez <julio.gteran@gmail.com>
2016-11-02 13:07:41 +0000

    Log in/New user navigation buttons

---
+++ spec/features/navigation_bar_spec.rb
@@ -0,0 +1,13 @@
+feature 'navigation buttons' do
+  scenario 'clicking logging button' do
+    visit '/'
+    click_button 'Log In'
+    expect(current_path).to eq('/sessions/new')
+  end
+
+  scenario 'clicking New User button' do
+    visit '/'
+    click_button 'New User'
+    expect(current_path).to eq('/user/new')
+  end
+end

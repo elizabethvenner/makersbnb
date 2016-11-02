271438db1b8a595867fdddd250ff307869d9d926
Felix Harrison <fmlharrison@gmail.com>
2016-11-02 14:49:43 +0000

    added sign out test

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -24,12 +24,13 @@ feature 'User sign-up' do
     expect(page).to have_content 'Please enter a correct email address'
   end
 
-  # scenario 'Cannot sign up with a email that is already in the system' do
-  #   user_signup
-  #   expect{user_signup(email: "j.smith@aol.com")}.not_to change(User, :count)
-  #   expect(current_path).to eq('/user')
-  #   expect(page).to have_content 'This email is already in use'
-  # end
+  scenario 'Cannot sign up with a email that is already in the system' do
+    user_signup
+    click_button('Sign Out')
+    expect{user_signup(email: "j.smith@aol.com")}.not_to change(User, :count)
+    expect(current_path).to eq('/user')
+    expect(page).to have_content 'This email is already in use'
+  end
 
   scenario 'Cannot create a new user when logged in' do
     user_signup

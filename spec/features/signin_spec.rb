393946090c95763f33694c9befdc4376ce4dc253
Felix Harrison <fmlharrison@gmail.com>
2016-11-02 10:57:24 +0000

    added tests for signing in with incorrect details

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -8,11 +8,16 @@ feature 'user signin' do
     end
 
   scenario 'user can sign in' do
-    p user.email
-    p user.password
     signin(email: user.email, password: user.password)
 
     expect(page.status_code).to eq(200)
     expect(page).to have_content "Welcome #{user.first_name}"
+  end
+
+  scenario 'user can\'t sign in with an incorrect email or password' do
+    signin(email: "jenny.smith@hotmail.com", password: "bau123")
+    expect(current_path).to eq('/sessions')
+    expect(page).to have_content("The email or password is incorrect")
+
   end
 end

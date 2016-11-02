4e3eba365bd2f37dc6dd139cc8a7d5eed55472d1
Felix Harrison <fmlharrison@gmail.com>
2016-11-02 10:48:34 +0000

    added features for email validations

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -12,6 +12,23 @@ feature 'User sign-up' do
     expect(page).to have_content 'Sorry, Passwords mismatch!!'
   end
 
-  
+  scenario 'a email must be given' do
+    expect{user_signup(email: nil)}.not_to change(User, :count)
+    expect(current_path).to eq('/user')
+    expect(page).to have_content 'Email is mandatory'
+  end
+
+  scenario 'email must be entered in the correct format' do
+    expect{user_signup(email: 'thisisawrongemail')}.not_to change(User, :count)
+    expect(current_path).to eq('/user')
+    expect(page).to have_content 'Please enter a correct email address'
+  end
+
+  scenario 'Cannot sign up with a email that is already in the system' do
+    user_signup
+    expect{user_signup(email: "j.smith@aol.com")}.not_to change(User, :count)
+    expect(current_path).to eq('/user')
+    expect(page).to have_content 'This email is already in use'
+  end
 
 end

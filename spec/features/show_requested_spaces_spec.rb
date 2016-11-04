2860899e05d4bfb0e5984f0c1f04e4c94dbb36d5
ManuCiao <mn.sabatino@gmail.com>
2016-11-04 12:26:31 +0000

    Add Navigation bar CSS and fixed the rspec

--- spec/features/show_requested_spaces_spec.rb
+++ spec/features/show_requested_spaces_spec.rb
@@ -13,11 +13,11 @@ feature 'show requested spaces' do
  scenario 'when another user requests my space' do
    signin(email: user1.email, password: user1.password)
    add_space
-   click_button 'Sign Out'
+   click_button 'Log Out'
    user2_signup
    fill_in :check_in, with: "06/11/2016"
    click_button 'Request space'
-   click_button 'Sign Out'
+   click_button 'Log Out'
    signin(email: user1.email, password: user1.password)
    visit '/sessions/user/spaces/requests'
    find_button('Confirm').click
